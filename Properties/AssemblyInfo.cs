8ab89ee88c54fc3f48768a4f2a07bc4a954ac336
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-02-09 10:18:31 +0100

    Reference BusinessObjects package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FatturaElettronicaPA.PCL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FatturaElettronicaPA")]
+[assembly: AssemblyDescription("Fattura Elettronica per la Pubblica Amministrazione")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CIR 2000")]
-[assembly: AssemblyProduct("FatturaElettronicaPA.PCL")]
-[assembly: AssemblyCopyright("Copyright © CIR 2000 2014")]
+[assembly: AssemblyCompany("Nicola Iarocci, CIR2000")]
+[assembly: AssemblyProduct("FatturaElettronicaPA")]
+[assembly: AssemblyCopyright("Copyright © CIR2000 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -24,5 +24,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
