c5cc772d9efb0c9b2632ea67dcce48c372bb1058
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-07-15 14:56:09 +0200

    2.4.2.2 ModalitaPagamento: non convalida valore MP05.

--- Validators/FModalitaPagamentoValidator.cs
+++ Validators/FModalitaPagamentoValidator.cs
@@ -19,7 +19,7 @@ namespace FatturaElettronicaPA.Validators
         {
             Domain = new[]
             {
-                "MP01", "MP02", "MP03", "MP04", "MP06", "MP07", "MP08", "MP09", "MP10", "MP11", "MP12", "MP13",
+                "MP01", "MP02", "MP03", "MP04", "MP05", "MP06", "MP07", "MP08", "MP09", "MP10", "MP11", "MP12", "MP13",
                 "MP14", "MP15", "MP16", "MP17", "MP18", "MP19", "MP20", "MP21"
             };
         }
