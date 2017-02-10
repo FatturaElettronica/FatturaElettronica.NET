bdc18f11a6ea1cfdfc9db444e2f9991ae6e5bee3
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-02-10 09:42:55 +0100

    Improve reflection code for portability across platforms

--- FatturaElettronicaExtensions.cs
+++ FatturaElettronicaExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using FatturaElettronica;
-using FatturaElettronica.Validators;
-using FluentValidation;
 using FluentValidation.Results;
 
 namespace FatturaElettronica
@@ -14,8 +11,9 @@ namespace FatturaElettronica
             var type = Type.GetType(
                 string.Format("FatturaElettronica.Validators.{0}Validator", obj.GetType().Name));
             var instance = Activator.CreateInstance(type);
-            var method = type.GetMethod("Validate", new [] {obj.GetType()});
+            var method = type.GetRuntimeMethod("Validate", new[] { obj.GetType() });
             return (ValidationResult)method.Invoke(instance, new [] {obj} );
+
         }
     }
 }
