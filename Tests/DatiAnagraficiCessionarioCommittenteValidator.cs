54fa43126500ccf37622e26f512a92648c20dec5
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-02-16 10:15:21 +0100

    Add validation of error 00417 in CessionarioCommittente

---
+++ Tests/DatiAnagraficiCessionarioCommittenteValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DatiAnagraficiCessionarioCommittenteValidator 
+        : BaseDatiAnagraficiValidator<FatturaElettronica.Common.DatiAnagrafici, FatturaElettronica.Validators.DatiAnagraficiCessionarioCommittenteValidator>
+    {
+        [TestMethod]
+        public void IdFiscaleIVAIsOptional()
+        {
+            challenge.CodiceFiscale = "x";
+            Assert.IsTrue(challenge.IdFiscaleIVA.IsEmpty());
+
+            var r = validator.Validate(challenge);
+            Assert.IsNull(r.Errors.FirstOrDefault(x => x.PropertyName == "IdFiscaleIVA"));
+        }
+        [TestMethod]
+        public new void CodiceFiscaleIsOptional()
+        {
+            challenge.IdFiscaleIVA.IdCodice = "x";
+
+            AssertOptional(x => x.CodiceFiscale);
+        }
+        [TestMethod]
+        public void CodiceFiscaleOrIdFiscaleIVAMustHaveValue()
+        {
+            Assert.IsTrue(string.IsNullOrEmpty(challenge.CodiceFiscale));
+            Assert.IsTrue(challenge.IdFiscaleIVA.IsEmpty());
+
+            validator.ShouldHaveValidationErrorFor(x => x.CodiceFiscale, challenge).WithErrorCode("00417");
+        }
+    }
+}
---
+++ Validators/DatiAnagraficiCessionarioCommittente.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using FatturaElettronica.Common;
+
+namespace FatturaElettronica.Validators
+{
+    public class DatiAnagraficiCessionarioCommittenteValidator 
+        : DatiAnagraficiBaseValidator<DatiAnagrafici>
+    {
+        public DatiAnagraficiCessionarioCommittenteValidator()
+        {
+            RuleFor(x => x.CodiceFiscale)
+                .Must((challenge, _) => { return !(string.IsNullOrEmpty(challenge.CodiceFiscale) && challenge.IdFiscaleIVA.IsEmpty()); })
+                .WithMessage("IdFiscaleIVA e CodiceFiscale non valorizzati (almeno uno dei due deve essere valorizzato)")
+                .WithErrorCode("00417");
+            RuleFor(x => x.IdFiscaleIVA)
+                .Must((challenge, _) => { return !(string.IsNullOrEmpty(challenge.CodiceFiscale) && challenge.IdFiscaleIVA.IsEmpty()); })
+                .WithMessage("IdFiscaleIVA e CodiceFiscale non valorizzati (almeno uno dei due deve essere valorizzato)")
+                .WithErrorCode("00417");
+        }
+    }
+}
