fc9e70e1a5877d42276e6a17ba118b2d1d0c7d48
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-03-27 16:52:22 +0200

    Tabella.Codice e .Nome non sono più protetti in scrittura

--- Tabelle/Tabella.cs
+++ Tabelle/Tabella.cs
@@ -4,8 +4,8 @@ namespace FatturaElettronica.Tabelle
 {
     public abstract class Tabella
     {
-        public string Nome { get; protected set; }
-        public string Codice { get; protected set; }
+        public string Nome { get; set; }
+        public string Codice { get; set; }
         public string Descrizione { get { return Codice + " " + Nome; } }
         public string[] Codici
         {
