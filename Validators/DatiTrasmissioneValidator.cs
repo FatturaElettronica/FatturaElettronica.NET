0c9a88df8914405820ea646af7b2708077236fd8
brux88 <brux88@gmail.com>
2018-06-07 09:44:17 +0200

    Disabilitato ruolo che mi obbliga ad inserire la pec quando il codice destinatario  = 000000

--- Validators/DatiTrasmissioneValidator.cs
+++ Validators/DatiTrasmissioneValidator.cs
@@ -31,11 +31,11 @@ namespace FatturaElettronica.Validators
             RuleFor(dt => dt.ContattiTrasmittente)
                 .SetValidator(new ContattiTrasmittenteValidator())
                 .When(x=>!x.ContattiTrasmittente.IsEmpty());
-            RuleFor(dt => dt.PECDestinatario)
+           /* RuleFor(dt => dt.PECDestinatario)
                 .NotEmpty()
                 .When(dt => dt.CodiceDestinatario == "0000000")
                 .WithMessage("{PropertyName} non valorizzato a fronte di Codice Destinatario con valore 0000000")
-                .WithErrorCode("00426");
+                .WithErrorCode("00426");*/
             RuleFor(dt => dt.PECDestinatario)
                 .Length(7, 256);
             RuleFor(dt => dt.PECDestinatario)
