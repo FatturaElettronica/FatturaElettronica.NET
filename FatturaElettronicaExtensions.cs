ab6ea64dba6516594c2b8c5a8463070442b6e511
Puma <puma_work@hotmail.com>
2020-02-07 10:32:40 +0100

    Modifica validator per gestire sia classi astratte che non

--- FatturaElettronicaExtensions.cs
+++ FatturaElettronicaExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using FluentValidation;
 using FluentValidation.Results;
+using System.Reflection;
 
 namespace FatturaElettronica
 {
@@ -10,8 +11,9 @@ namespace FatturaElettronica
         private static readonly ConcurrentDictionary<string, IValidator> ValidatorsCache = new ConcurrentDictionary<string, IValidator>();
         public static ValidationResult Validate<T>(this T obj) where T : Common.BaseClassSerializable
         {
-            var t = obj.GetType();
-            string name = (t.FullName.Contains("Semplificata") ? "Semplificata." : string.Empty) + t.Name;
+            var t = typeof(T);
+            if(t.GetTypeInfo().IsAbstract) t = obj.GetType();
+            var name = (t.FullName.Contains("Semplificata") ? "Semplificata." : string.Empty) + t.Name;
 
             var validator = ValidatorsCache.GetOrAdd(name, n =>
             {
