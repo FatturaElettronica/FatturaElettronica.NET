acbd8dcf3baf984f7e3507495bd7fb6a08784da3
Nicola Iarocci <nicola@nicolaiarocci.com>
2020-03-04 10:12:03 +0100

    Semplificazione messaggi per codici 00443 e 0044
    Addresses #243

--- Validators/FatturaOrdinariaValidator.cs
+++ Validators/FatturaOrdinariaValidator.cs
@@ -15,17 +15,11 @@ namespace FatturaElettronica.Validators
                 .SetValidator(new FatturaElettronicaBodyValidator());
             RuleForEach(x => x.FatturaElettronicaBody)
                 .Must((_, body) => BodyValidateAgainstError00443(body))
-                .WithMessage(
-                    "Non c’è corrispondenza tra i valori indicati nell’elemento 2.2.1.12 <AliquotaIVA> o 2.1.1.7.5 <AliquotaIVA> e quelli" +
-                    "dell’elemento 2.2.2.1 <AliquotaIVA> (tutti i valori delle aliquote IVA presenti nelle linee di dettaglio di una fattura" +
-                    "o nei dati di cassa previdenziale devono essere presenti anche nei dati di riepilogo)")
+                .WithMessage("Tutti i valori delle aliquote IVA presenti nelle linee di dettaglio di una fattura o nei dati di cassa previdenziale devono essere presenti anche nei dati di riepilogo")
                 .WithErrorCode("00443");
             RuleForEach(x => x.FatturaElettronicaBody)
                 .Must((_, body) => BodyValidateAgainstError00444(body))
-                .WithMessage(
-                    "non c’è corrispondenza tra i valori indicati nell’elemento 2.2.1.14 <Natura> o 2.1.1.7.7 <Natura> e quelli" +
-                    " dell’elemento 2.2.2.2 <Natura> (tutti i valori di natura dell’operazione presenti nelle linee di dettaglio" +
-                    " di una fattura o nei dati di cassa previdenziale devono essere presenti anche nei dati di riepilogo)")
+                .WithMessage("Tutti i valori di natura dell’operazione presenti nelle linee di dettaglio di una fattura o nei dati di cassa previdenziale devono essere presenti anche nei dati di riepilogo")
                 .WithErrorCode("00444");
         }
 
