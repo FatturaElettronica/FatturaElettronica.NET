d19ac498b9500f5c376d1cf3e743b0be270ace87
Emanuele Zavallone <emanuele.zavallone@next-informatica.com>
2020-03-06 15:16:54 +0100

    Specifiche 1.3 - Aggiornamento  tabelle Modalita pagamento, Natura, tipo ritenuta

--- Tabelle/TipoRitenuta.cs
+++ Tabelle/TipoRitenuta.cs
@@ -7,8 +7,12 @@
             get
             {
                 return new Tabella[] {
-                    new TipoRitenuta { Codice = "RT01", Nome = "ritenuta persone fisiche" },
-                    new TipoRitenuta { Codice = "RT02", Nome = "ritenuta persone giuridiche" },
+                    new TipoRitenuta { Codice = "RT01", Nome = "Ritenuta persone fisiche" },
+                    new TipoRitenuta { Codice = "RT02", Nome = "Ritenuta persone giuridiche" },
+                    new TipoRitenuta { Codice = "RT03", Nome = "Contributo INPS" },
+                    new TipoRitenuta { Codice = "RT04", Nome = "Contributo ENASARCO" },
+                    new TipoRitenuta { Codice = "RT05", Nome = "Contributo ENPAM" },
+                    new TipoRitenuta { Codice = "RT06", Nome = "Altro contributo previdenziale" }
                 };
             }
         }
