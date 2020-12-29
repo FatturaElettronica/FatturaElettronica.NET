79b0d38d35692c788e2d5a234947525ee21f8653
Nicola Iarocci <nicola@nicolaiarocci.com>
2020-12-29 16:28:53 +0100

    refactor ExtraAttributes as an auto-property

--- Defaults.cs
+++ Defaults.cs
@@ -18,18 +18,13 @@
     {
         public static string Prefix { get; set; } = "p";
 
-        private static XmlAttributeString[] _extraAttributes = new XmlAttributeString[] {
-                    new XmlAttributeString { Prefix="xmlns", LocalName="ds", ns=null, value="http://www.w3.org/2000/09/xmldsig#"},
-                    new XmlAttributeString { Prefix="xmlns", LocalName="xsi", ns=null, value="http://www.w3.org/2001/XMLSchema-instance"},
-                    new XmlAttributeString { Prefix="xsi", LocalName="schemaLocation", ns=null, value="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2 https://www.fatturapa.gov.it/export/documenti/fatturapa/v1.2.1/Schema_del_file_xml_FatturaPA_versione_1.2.1a.xsd"}
-                };
 
+        public static XmlAttributeString[] ExtraAttributes { get; set; } = new XmlAttributeString[] {
+            new XmlAttributeString { Prefix="xmlns", LocalName="ds", ns=null, value="http://www.w3.org/2000/09/xmldsig#"},
+            new XmlAttributeString { Prefix="xmlns", LocalName="xsi", ns=null, value="http://www.w3.org/2001/XMLSchema-instance"},
+            new XmlAttributeString { Prefix="xsi", LocalName="schemaLocation", ns=null, value="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2 https://www.fatturapa.gov.it/export/documenti/fatturapa/v1.2.1/Schema_del_file_xml_FatturaPA_versione_1.2.1a.xsd"}
+        };
 
-        public static XmlAttributeString[] ExtraAttributes
-        {
-            get => _extraAttributes;
-            set => _extraAttributes = value;
-        }
         public class XmlAttributeString
         {
             public string Prefix;
