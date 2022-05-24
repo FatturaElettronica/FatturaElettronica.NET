a854abb43a0d2b3c0a0e80139adb36d4fe1d4658
Michael Mairegger <michael.mairegger@yahoo.de>
2022-05-24 09:33:34 +0200

    [Fix] WriteHtml does not create a temp file

--- Extensions/HtmlExtensions.cs
+++ Extensions/HtmlExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Xml;
+using System.Xml.XPath;
 using System.Xml.Xsl;
 
 namespace FatturaElettronica.Extensions
@@ -13,22 +15,31 @@ namespace FatturaElettronica.Extensions
             {
                 throw new ArgumentNullException(nameof(outPath));
             }
-
+            
             if (xslPath == null)
             {
                 throw new ArgumentNullException(nameof(xslPath));
             }
 
-            var tempXml = Path.GetTempFileName();
-
-            using (var w = XmlWriter.Create(tempXml, new() {Indent = true}))
-            {
-                fattura.WriteXml(w);
-            }
-
             var xt = new XslCompiledTransform();
             xt.Load(xslPath);
-            xt.Transform(tempXml, outPath);
+            
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var w = new XmlTextWriter(memoryStream, Encoding.UTF8) {Formatting = Formatting.Indented})
+                {
+                    fattura.WriteXml(w);
+                    w.Flush();
+                    
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    var xPathDocument = new XPathDocument(memoryStream);
+                    using (var writer = File.OpenWrite(outPath))
+                    {
+                        xt.Transform(xPathDocument, null, writer);
+                    }
+                }
+            }
         }
     }
 }
